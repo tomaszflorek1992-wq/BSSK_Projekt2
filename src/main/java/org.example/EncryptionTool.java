0bb35e22d6f508d552f0fd9458dfe6af2166ebd7
Tomek <tomasz.florek1992@gmail.com>
2026-05-17 12:32:26 +0200

    dodanie Encryption Tool

---
+++ src/main/java/org.example/EncryptionTool.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionTool {
+    private static final String ALGORITHM = "AES";
+
+
+    public static String decrypt(String encryptedData, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decoded = Base64.getDecoder().decode(encryptedData);
+        return new String(cipher.doFinal(decoded));
+    }
+
+    public static String encrypt(String data, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+}
