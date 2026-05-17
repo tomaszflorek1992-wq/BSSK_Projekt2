8ce8aa3e0884e88543b4dcb4b624a3551de69235
Tomek <tomasz.florek1992@gmail.com>
2026-05-17 13:01:55 +0200

    dodanie klasy main

---
+++ src/main/java/org.example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        try {
+            DatabaseManager dbManager = new DatabaseManager();
+
+            dbManager.setupDatabase();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("=== SYSTEM BEZPIECZNEJ REJESTRACJI ===");
+
+            System.out.print("Podaj nazwę użytkownika: ");
+            String username = scanner.nextLine();
+
+            System.out.print("Podaj hasło: ");
+            String password = scanner.nextLine();
+
+            dbManager.saveUser(username, password);
+
+            System.out.println("=== Koniec programu ===");
+
+        } catch (Exception e) {
+            System.err.println("BŁĄD KRYTYCZNY: " + e.getMessage());
+            System.err.println("Upewnij się, że ustawiono MASTER_KEY w zmiennych środowiskowych!");
+        }
+    }
+}
